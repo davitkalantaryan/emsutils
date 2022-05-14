a5155145a7e3cfdf527216081702484a4450afa2
Davit Kalantaryan <davit.kalantaryan@desy.de>
2022-05-14 17:12:14 +0200

    Develop (#112)
    * Fixed internal header
    * Fixed disabling the newer warning
    * more check is done to disable the recent warning

--- include/cpputils/disable_compiler_warnings.h
+++ include/cpputils/disable_compiler_warnings.h
@@ -14,7 +14,9 @@
 
 #pragma warning(disable:4710)
 #pragma warning(disable:4711)
+#if (_MSC_VER>=1929) && (_MSC_FULL_VER>192930100)
 #pragma warning(disable:5246)
+#endif
 
 #elif defined(__GNUC__) 
 
