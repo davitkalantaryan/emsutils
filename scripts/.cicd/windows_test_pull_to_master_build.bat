d1a704bc7d80acdd144f0efe3c0316dfd5ed7321
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-26 16:44:39 +0200

    fixed the file scripts/.cicd/windows_test_pull_to_master_build.bat

--- scripts/.cicd/windows_test_pull_to_master_build.bat
+++ scripts/.cicd/windows_test_pull_to_master_build.bat
@@ -12,28 +12,10 @@ set scriptDirectory=%~dp0
 cd /D %scriptDirectory%..\..
 set "repositoryRoot=%cd%\"
 
-cd "%repositoryRoot%prj\tests\googletest_mult"
-rem msbuild -t:restore -p:RestorePackagesConfig=true
-rem if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
-nmake -f windows.Makefile Platform=!Platform! Configuration=!Configuration!
+cd "%repositoryRoot%prj\tests\cpputils_unit_test_mult"
 if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
-
-rem cd "%repositoryRoot%workspaces\cpputils_vs"
-rem call msbuild "%repositoryRoot%workspaces\cpputils_vs\cpputils.sln" /t:%ActionConfirm% /p:Configuration=%Configuration% /p:Platform=%Platform% -m:2
-rem if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
-
-rem do we need to compile makefile projects, then init env and do
-rem if not "!VSCMD_ARG_HOST_ARCH!"=="!Platform!" (
-rem 	echo "calling VsDevCmd in the !scriptDirectory!!scriptName!"
-rem 	if /i "!Platform!"=="x64" (
-rem 		set "PlatformTarget=amd64"
-rem 	) else (
-rem 		set "PlatformTarget=!Platform!"
-rem 	)
-rem 	call VsDevCmd -arch=!PlatformTarget!
-rem ) else (
-rem 	echo "VsDevCmd already set to !Platform!"
-rem )
+call nmake -f cpputils_unit_test.windows.Makefile Platform=!Platform! Configuration=!Configuration!
+if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 
 
 exit /b 0
