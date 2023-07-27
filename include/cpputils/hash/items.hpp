23cb18959a0d0fe7a413bd1b42ff19d814eda690
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-27 22:31:37 +0200

    suppressed some meaningless warning

--- include/cpputils/hash/items.hpp
+++ include/cpputils/hash/items.hpp
@@ -18,6 +18,7 @@ namespace cpputils { namespace hash {
 
 #ifdef _MSC_VER
 #pragma warning (disable:4820)  //  '4' bytes padding added after data ...
+#pragma warning (disable:4514)  //  unreferenced inline function has been removed
 #endif
 
 #define CPPUTILS_HASH_DEFAULT_TABLE_SIZE	256
