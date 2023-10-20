5b3cf26858d55be85c0f889cea8b23970812ba87
Davit Kalantaryan <davit.kalantaryan@desy.de>
2023-10-20 21:28:02 +0200

    simplified scripts/unix_prepare_repository.sh file

--- scripts/unix_prepare_repository.sh
+++ scripts/unix_prepare_repository.sh
@@ -25,22 +25,6 @@ cd ..
 emsutilsRepoRoot=`pwd`
 echo emsutilsRepoRoot=$emsutilsRepoRoot
 
-# thanks to https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
-if [[ "$(uname)" == "Darwin" ]]; then
-	# Do something under Mac OS X platform
-	lsbCode=mac
-	qtTarget=clang_64
-elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-	# Do something under GNU/Linux platform
-	lsbCode=`lsb_release -sc`
-	qtTarget=gcc_64
-#elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]]; then
-#	# Do something under 32 bits Windows NT platform
-#elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]; then
-#	# Do something under 64 bits Windows NT platform
-#else
-fi
-
 git submodule sync --recursive
 git submodule update --init --recursive
 
